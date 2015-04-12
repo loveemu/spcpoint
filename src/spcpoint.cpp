0edf827c21648ea97ca7ca54c86616704681d4c7
loveemu <loveemu@gmail.com>
2015-04-12 13:54:10 +0900

    Add `-tf` implementation

--- src/spcpoint.cpp
+++ src/spcpoint.cpp
@@ -20,6 +20,20 @@
 #define APP_VER     "[2015-04-09]"
 #define APP_URL     "http://github.com/loveemu/spcpoint"
 
+bool both_are_spaces(char lhs, char rhs)
+{
+	return (lhs == rhs) && (lhs == ' ');
+}
+
+void replace_all(std::string& str, const std::string& from, const std::string& to)
+{
+	std::string::size_type pos = 0;
+	while (pos = str.find(from, pos), pos != std::string::npos) {
+		str.replace(pos, from.length(), to);
+		pos += to.length();
+	}
+}
+
 static void usage(const char * progname)
 {
 	printf("%s %s\n", APP_NAME, APP_VER);
@@ -37,6 +51,7 @@ int main(int argc, char *argv[])
 	}
 
 	std::map<std::string, std::string> opt_tags;
+	bool title_from_filename = false;
 
 	int argi = 1;
 	while (argi < argc && argv[argi][0] == '-')
@@ -54,6 +69,9 @@ int main(int argc, char *argv[])
 			if (strcmp(argv[argi], "--help") == 0) {
 				usage(argv[0]);
 				return EXIT_FAILURE;
+			}
+			else if (strcmp(argv[argi], "-tf") == 0) {
+				title_from_filename = true;
 			}
 			else {
 				fprintf(stderr, "Error: Unknown option \"%s\"\n", argv[argi]);
@@ -93,7 +111,35 @@ int main(int argc, char *argv[])
 		printf("\n");
 #endif
 
-		spc->ImportPSFTag(opt_tags);
+		std::map<std::string, std::string> psf_tags(opt_tags);
+		if (title_from_filename) {
+			std::string title(argv[argi]);
+
+			// remove extension
+			std::string::size_type offset_dot = title.find_last_of('.');
+			if (offset_dot != std::string::npos) {
+				title = title.substr(0, offset_dot);
+			}
+
+			// trim some beginning characters
+			std::string::size_type offset_start = title.find_first_not_of(" _%0123456789-");
+			if (offset_start != std::string::npos) {
+				title = title.substr(offset_start);
+			}
+
+			// replace some other stuff
+			replace_all(title, "%20", " ");
+			replace_all(title, "_", " ");
+
+			// replace multiple spaces with one space
+			std::string::iterator new_end = std::unique(title.begin(), title.end(), both_are_spaces);
+			title.erase(new_end, title.end());
+
+			psf_tags["title"] = title;
+			puts(title.c_str());
+		}
+
+		spc->ImportPSFTag(psf_tags);
 		spc->Save(argv[argi]);
 
 		delete spc;
